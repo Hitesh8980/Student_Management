3c1742959643bee8fc7821dbe9888236ff37add7
Hitesh Sharma <hitesh.sharma176@gmail.com>
2024-09-18 17:20:44 +0530

    Remove .history directory from repo

--- .history/Front-end/src/components/QrcodeGenerator_20240918081952.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { QRCodeSVG } from 'qrcode.react'; // Assuming you're using this package for QR generation
-
-function QRCodeGenerator({ value }) {
-  return <QRCodeSVG value={value} />;
-}
-
-export default QRCodeGenerator;
--- .history/Front-end/src/components/QrcodeGenerator_20240918081955.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { QRCodeSVG } from 'qrcode.react'; 
-
-function QRCodeGenerator({ value }) {
-  return <QRCodeSVG value={value} />;
-}
-
-export default QRCodeGenerator;
